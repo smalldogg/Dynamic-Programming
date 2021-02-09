77b95baea5f2b5338cb8159050a9f12414f6edac
wangyinhui <13037651897@163.com>
2021-02-09 23:08:47 +0800

    优化代码

--- src/入门/跳跃游戏2.java
+++ src/入门/跳跃游戏2.java
@@ -6,9 +6,7 @@ package 入门;
  */
 public class 跳跃游戏2 {
 		public int jump( int[] A ) {
-				if( A == null || A.length == 0 ) {
-						return 0;
-				}
+				if( A == null || A.length == 0 ) return 0;
 				int n = A.length;
 				int[] dp = new int[n];
 				dp[0] = 0;
@@ -16,9 +14,8 @@ public class 跳跃游戏2 {
 				for( i = 1; i < n; i++ ) {
 						dp[i] = Integer.MAX_VALUE;
 						for( j = 0; j < i; j++ ) {
-								if( dp[j] != Integer.MAX_VALUE && A[j] + j >= i ) {
+								if( dp[j] != Integer.MAX_VALUE && A[j] + j >= i )
 										dp[i] = Math.min( dp[i], dp[j] + 1 );
-								}
 						}
 				}
 				return dp[n - 1];
